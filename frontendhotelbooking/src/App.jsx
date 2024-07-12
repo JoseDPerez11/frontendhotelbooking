7b55cfd97b2567a10450361383f4ee856f69f66e
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-07-12 14:09:26 -0500

    booking success and form ui

--- frontendhotelbooking/src/App.jsx
+++ frontendhotelbooking/src/App.jsx
@@ -11,6 +11,8 @@ import Footer from "./components/layout/Footer"
 import NavBar from './components/layout/NavBar'
 import RoomListing from './components/room/RoomListing'
 import Admin from "./components/admin/Admin"
+import Checkout from './components/booking/Checkout'
+import BookingSuccess from './components/booking/BookingSuccess'
 
 function App() {
   return(
@@ -26,8 +28,10 @@ function App() {
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path='/add-room' element={<AddRoom />} />
+            <Route path='/book-room/:roomId' element={<Checkout />} />
             <Route path='/browse-all-rooms' element={<RoomListing />} />
             <Route path='/admin' element={<Admin />} />
+            <Route path="/booking-success" element={<BookingSuccess />} />
             
           </Routes>
         </Router>
